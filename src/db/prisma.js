528fb7c77b50a83ef481e8ec8f7ec21b5ed35d1b
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-04 21:45:49 +0100

    feat(db)!: Session model to support the PrismaSessionStore to save cookies

--- src/db/prisma.js
+++ src/db/prisma.js
@@ -1,5 +1,7 @@
 import process from "process";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+
 import { PrismaClient } from "../generated/prisma/client.ts";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
@@ -14,6 +16,12 @@ const prisma = new PrismaClient({
             status: true,
         },
     },
+});
+
+export const prismaSessionStore = new PrismaSessionStore(prisma, {
+    checkPeriod: 2 * 60 * 1000,
+    dbRecordIdIsSessionId: true,
+    dbRecordIdFunction: undefined,
 });
 
 export default prisma;
