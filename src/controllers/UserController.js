c986d0fb33385beb093473cb8b55c4b7aa9b4d59
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-04 20:21:09 +0100

    feat: GET /user/

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,3 +1,15 @@
-class UserController {}
+import prisma from "../db/prisma.js";
+
+class UserController {
+    async getUsers(req, res) {
+        const users = await prisma.user.findMany({
+            include: {
+                userProfile: true,
+            },
+        });
+
+        return res.json(users);
+    }
+}
 
 export default new UserController();
