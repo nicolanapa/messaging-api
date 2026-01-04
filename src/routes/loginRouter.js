26240850692c510718e6b43338e912dab6083728
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-04 20:28:56 +0100

    feat: Linking of POST /login/ and POST /user/ to the methods of AuthorizationController

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import authorizationController from "../controllers/AuthorizationController.js";
+
 const loginRouter = Router();
+
+loginRouter.post("/", authorizationController.login);
 
 export default loginRouter;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
+
 import userController from "../controllers/userController.js";
+import authorizationController from "../controllers/AuthorizationController.js";
 
 const userRouter = Router();
 
 userRouter.get("/", userController.getUsers);
+
+userRouter.post("/", authorizationController.signUp);
 
 export default userRouter;
