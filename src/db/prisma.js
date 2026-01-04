5a02855ebe6aa238542688cb64d79bd8a58356eb
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-04 20:20:12 +0100

    feat(db): Omit option to omit fields behind authorization

--- src/db/prisma.js
+++ src/db/prisma.js
@@ -3,6 +3,17 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.ts";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+    adapter,
+    omit: {
+        user: {
+            email: true,
+            hashedPassword: true,
+        },
+        userProfile: {
+            status: true,
+        },
+    },
+});
 
 export default prisma;
