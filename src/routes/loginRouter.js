9d6d600b21d0145929b2e3c1eba48bb54bdd1371
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-04 21:34:13 +0100

    feat: Authentication via Passport in POST /login/

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 
+import passport from "../db/passport.js";
 import authorizationController from "../controllers/AuthorizationController.js";
 
 const loginRouter = Router();
 
-loginRouter.post("/", authorizationController.login);
+loginRouter.post(
+    "/",
+    passport.authenticate("local"),
+    authorizationController.login,
+);
 
 export default loginRouter;
