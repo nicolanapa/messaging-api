10f109e0fde0468210917ce591c2b68afe6a3f77
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-04 20:27:51 +0100

    feat: Renamed the LoginController to allow more usage out of it

---
+++ src/controllers/AuthorizationController.js
@@ -0,0 +1,7 @@
+class AuthorizationController {
+    async login(req, res) {}
+
+    async signUp(req, res) {}
+}
+
+export default new AuthorizationController();
--- src/controllers/LoginController.js
+++
@@ -1,3 +0,0 @@
-class LoginController {}
-
-export default new LoginController();
