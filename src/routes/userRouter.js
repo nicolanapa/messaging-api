43c6eb1978dc1b22814bc7ac0c86332cff9747ca
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-05 21:32:57 +0100

    feat: userValidator usage in POST /user/'s parameters

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 
 import userController from "../controllers/userController.js";
 import authorizationController from "../controllers/AuthorizationController.js";
+import { userValidator } from "../scripts/userValidator.js";
 
 const userRouter = Router();
 
 userRouter.get("/", userController.getUsers);
 
-userRouter.post("/", authorizationController.signUp);
+userRouter.post("/", userValidator, authorizationController.signUp);
 
 userRouter.get("/:id", userController.getUser);
 
