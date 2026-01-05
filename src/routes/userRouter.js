2e10d6ba13712e2f52dd83cb57ad62ad368b29da
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-05 21:40:33 +0100

    feat: Filled with the remaining User's routes, controller's methods not yet updated

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -12,4 +12,33 @@ userRouter.post("/", userValidator, authorizationController.signUp);
 
 userRouter.get("/:id", userController.getUser);
 
+userRouter.delete(
+    "/:id",
+    /* passport + identify user helper, 
+    also used in most of the other routes ,*/ authorizationController.signUp,
+);
+
+userRouter.put("/:id/update", /* passport ,*/ authorizationController.signUp);
+
+userRouter.get("/:id/status", /* passport ,*/ authorizationController.signUp);
+
+userRouter.put("/:id/status", /* passport ,*/ authorizationController.signUp);
+
+userRouter.get("/:id/publicKey", authorizationController.signUp);
+
+userRouter.put(
+    "/:id/publicKey",
+    /* passport ,*/ authorizationController.signUp,
+);
+
+// Friend requests and friend list routes, not yet decided
+
+// userRouter.get("/:id/blockedList", /* passport ,*/ authorizationController.signUp);
+
+// userRouter.put("/:id/block", /* passport ,*/ authorizationController.signUp);
+
+// userRouter.put("/:id/unblock", /* passport ,*/ authorizationController.signUp);
+
+userRouter.get("/:id/chat", /* passport ,*/ authorizationController.signUp);
+
 export default userRouter;
