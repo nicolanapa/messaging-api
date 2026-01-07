a0fe471b83805541d4d9d7a0c34b9c9898632799
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-07 21:48:30 +0100

    feat: Initial format of UserController's deleteUser method with simple deletion of a User

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -24,7 +24,46 @@ class UserController {
         return res.json(users);
     }
 
-    async deleteUser(req, res) {}
+    async deleteUser(req, res) {
+        // Deletion process to be improved (and tested) when chats are working
+
+        /*const userChats = await prisma.chatParticipation.findMany({
+            where: {
+                userId: req.params.id,
+            },
+        });
+
+        if (userChats !== null) {
+            for (let i = 0; i < userChats.length; i++) {
+                // Search every single chats for the mentioned checks
+            }
+        }*/
+
+        await prisma.user.delete({
+            where: {
+                id: req.params.id,
+            },
+            include: {
+                userProfile: true,
+                userBlocker: true,
+                blockedFromUsers: true,
+                chatsParticipatingIn: true,
+                chatsDeleted: true,
+                messages: false,
+                images: false,
+            },
+        });
+
+        // All messages are kept, the User's username will be 'Deleted User' or similar
+        // A single chat between another User will have a single participator
+        // They can send messages (?) or delete it permanently
+        // Group chats will have one less participator, if there's already
+        // one person then it will be deleted
+        // I'll have to implement other special cases and checks
+        // Otherwise If I do it now then it's like going blind in it
+
+        return res.status(200).json(true);
+    }
 
     async updateUser(req, res) {}
 
