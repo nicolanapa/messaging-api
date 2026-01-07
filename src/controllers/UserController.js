1582f26ce8f4219563dec48ca8f36ecaa3974565
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-07 21:06:44 +0100

    feat: Empty needed methods for the Controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -23,6 +23,22 @@ class UserController {
 
         return res.json(users);
     }
+
+    async deleteUser(req, res) {}
+
+    async updateUser(req, res) {}
+
+    async getUserStatus(req, res) {}
+
+    async updateUserStatus(req, res) {}
+
+    async getUserPublicKey(req, res) {}
+
+    async updateUserPublicKey(req, res) {}
+
+    // Some other methods
+
+    async getUserChats(req, res) {}
 }
 
 export default new UserController();
