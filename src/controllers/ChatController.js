acc669717db04499d43c7f58273662325538229b
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-17 18:01:36 +0100

    feat: Empty needed Controller's methods

--- src/controllers/ChatController.js
+++ src/controllers/ChatController.js
@@ -1,3 +1,19 @@
-class chatController {}
+class chatController {
+    async postChat(req, res) {}
+
+    async getChat(req, res) {}
+
+    async deleteChat(req, res) {}
+
+    async getUsers(req, res) {}
+
+    async addUser(req, res) {}
+
+    async getMessages(req, res) {}
+
+    async postMessage(req, res) {}
+
+    async deleteMessage(req, res) {}
+}
 
 export default new chatController();
