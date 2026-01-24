38f1551c188382d3f7a9ac2a7999b38a5e491f7d
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2026-01-24 19:11:03 +0100

    feat: DELETE /chat/:id

--- src/controllers/ChatController.js
+++ src/controllers/ChatController.js
@@ -1,9 +1,68 @@
+import prisma from "../db/prisma.js";
+
 class chatController {
     async postChat(req, res) {}
 
     async getChat(req, res) {}
 
-    async deleteChat(req, res) {}
+    async deleteChat(req, res) {
+        const chatParticipations = await prisma.chatParticipation.findMany({
+            where: {
+                chatId: req.params.id,
+            },
+        });
+
+        const chatDeletions = await prisma.chatDeletion.findMany({
+            where: {
+                chatId: req.params.id,
+            },
+        });
+
+        const isParticipant = chatParticipations.some(
+            (participation) => participation.userId === req.user,
+        );
+
+        const hasDeletedChat = chatDeletions.some(
+            (deletion) => deletion.userId === req.user,
+        );
+
+        /*console.log(
+            chatParticipations,
+            chatDeletions,
+            isParticipant,
+            hasDeletedChat,
+        );*/
+
+        if (isParticipant && !hasDeletedChat) {
+            await prisma.chatDeletion.create({
+                data: {
+                    chatId: req.params.id,
+                    userId: req.user,
+                },
+            });
+        }
+
+        /*console.log(
+            chatParticipations.length,
+            chatDeletions.length + (!hasDeletedChat ? 1 : 0),
+        );*/
+
+        // It works if there aren't duplicated records
+        if (
+            chatParticipations.length !==
+            chatDeletions.length + (!hasDeletedChat ? 1 : 0)
+        ) {
+            return res.status(202).json(false);
+        }
+
+        await prisma.chat.delete({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        return res.status(200).json(true);
+    }
 
     async getUsers(req, res) {}
 
